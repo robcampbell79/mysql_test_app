56f2489e1417fb17bbaf968f2fb0b4506d10ba60
Robert <robcampbell@ucmo.edu>
2019-03-06 11:37:09 -0600

    Add comment

--- app/MysqlModel.php
+++ app/MysqlModel.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB; //I had to add this line to use DB::table
 
 class MysqlModel extends Model
 {
